24f965631e0a1cde9fdffc38f4fbcf7a3f4bb6ab
SeungHwan Lee <thomas79.kr@gmail.com>
2014-04-22 15:32:01 +0900

    Initial Commit

---
+++ Study/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Study
+//
+//  Created by 이승환 on 2014. 4. 22..
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
